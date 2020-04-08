0c6d5b37f083d6d095dd196aa54f7a17c9cc6e85
air <asilkhan-al@mail.ru>
2020-04-08 17:56:29 +0600

    Config controller added

--- app/Http/Controllers/Web/ConfigController.php
+++ app/Http/Controllers/Web/ConfigController.php
@@ -9,65 +9,10 @@ use Illuminate\Support\Facades\Artisan;
 class ConfigController extends WebBaseController
 {
 
-    public function migrateRefresh(Request $request)
+    public function command($request)
     {
-        if ($request->token == 'kasya') {
-            return Artisan::call('migrate:refresh');
-        } else {
-            return 'fail';
-        }
-    }
-
-
-    public function optimize(Request $request)
-    {
-        if ($request->token == 'kasya') {
-            return Artisan::call('optimize');
-        } else {
-            return 'fail';
-        }
-    }
-
-    public function clearAutoLoad(Request $request)
-    {
-        if ($request->token == 'kasya') {
-            return Artisan::call('clear-compiled ') && Artisan::call('php artisan optimize');
-        } else {
-            return 'fail';
-        }
-    }
-
-    public function migrate(Request $request)
-    {
-        if ($request->token == 'kasya') {
-            return Artisan::call('migrate');
-        } else {
-            return 'fail';
-        }
-    }
-
-    public function keyGenerate(Request $request)
-    {
-        if ($request->token == 'kasya') {
-            return Artisan::call('key:generate');
-        } else {
-            return 'fail';
-        }
-    }
-
-    public function configCache(Request $request)
-    {
-        if ($request->token == 'kasya') {
-            return Artisan::call('config:cache');
-        } else {
-            return 'fail';
-        }
-    }
-
-    public function dbSeed(Request $request)
-    {
-        if ($request->token == 'kasya') {
-            return Artisan::call('db:seed');
+        if ($request->token == 'kasya' && $request->command) {
+            return Artisan::call($request->command);
         } else {
             return 'fail';
         }
