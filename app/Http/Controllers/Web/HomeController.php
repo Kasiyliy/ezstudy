833b108e0031a7012f5d6a4228512ba25a70c994
air <asilkhan-al@mail.ru>
2020-04-15 05:31:22 +0600

    Password update added

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\WebBaseController;
+use App\Models\Education\Course;
+use App\Models\Education\Lesson;
+use App\Models\Education\Question;
+use App\Models\Education\Quiz;
+use App\Models\Education\QuizResult;
 use App\Models\System\User;
 
 class HomeController extends WebBaseController
@@ -10,7 +15,14 @@ class HomeController extends WebBaseController
 
     public function index()
     {
-        return view('admin.main.index');
+        $courseCount = Course::count();
+        $lessonCount = Lesson::count();
+        $userCount = User::count();
+        $quizCount = Quiz::count();
+        $questionCount = Question::count();
+        $quizResultCount = QuizResult::count();
+        return view('admin.main.index',
+            compact('courseCount', 'lessonCount', 'quizCount', 'userCount', 'questionCount', 'quizResultCount'));
     }
 
 }
--- resources/views/admin/main/index.blade.php
+++ resources/views/admin/main/index.blade.php
@@ -2,7 +2,62 @@
 
 @section('content')
     <div class="content-wrapper">
-
+        <div class="row">
+            <div class="col-md-12 grid-margin">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-6">
+                                <div class="d-flex">
+                                    <div class="wrapper">
+                                        <h3 class="mb-0 font-weight-semibold">{{$courseCount}}</h3>
+                                        <h5 class="mb-0 font-weight-medium text-primary">Курстар саны</h5>
+                                    </div>
+                                    <div class="wrapper my-auto ml-auto ml-lg-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+                                        <canvas height="100" width="200" id="stats-line-graph-1" class="chartjs-render-monitor" style="display: block; height: 50px; width: 100px;"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 mt-md-0 mt-4">
+                                <div class="d-flex">
+                                    <div class="wrapper">
+                                        <h3 class="mb-0 font-weight-semibold">{{$lessonCount}}</h3>
+                                        <h5 class="mb-0 font-weight-medium text-primary">Сабақтар саны</h5>
+                                    </div>
+                                    <div class="wrapper my-auto ml-auto ml-lg-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+                                        <canvas height="100" width="200" id="stats-line-graph-2" class="chartjs-render-monitor" style="display: block; height: 50px; width: 100px;"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 mt-md-0 mt-4">
+                                <div class="d-flex">
+                                    <div class="wrapper">
+                                        <h3 class="mb-0 font-weight-semibold">{{$userCount}}</h3>
+                                        <h5 class="mb-0 font-weight-medium text-primary">Қолданушылар саны</h5>
+                                        <p class="mb-0 text-muted">Сертификат саны: {{$quizResultCount}}</p>
+                                    </div>
+                                    <div class="wrapper my-auto ml-auto ml-lg-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+                                        <canvas height="100" width="200" id="stats-line-graph-3" class="chartjs-render-monitor" style="display: block; height: 50px; width: 100px;"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 mt-md-0 mt-4">
+                                <div class="d-flex">
+                                    <div class="wrapper">
+                                        <h3 class="mb-0 font-weight-semibold">{{$quizCount}}</h3>
+                                        <h5 class="mb-0 font-weight-medium text-primary">Тест саны</h5>
+                                        <p class="mb-0 text-muted">Сұрақтар саны: {{$questionCount}}</p>
+                                    </div>
+                                    <div class="wrapper my-auto ml-auto ml-lg-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+                                        <canvas height="100" width="200" id="stats-line-graph-4" class="chartjs-render-monitor" style="display: block; height: 50px; width: 100px;"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 @endsection
